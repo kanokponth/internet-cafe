ca3fb2e1719fcb2d1adc504018135b5ef8fd3f7e
kanokponth <kanokpon.th@ku.th>
2018-09-12 12:04:21 +0700

    Initial commit

---
+++ src/main/java/Page1Controller.java
@@ -0,0 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Page1Controller {
+    @FXML
+    protected Button button;
+    @FXML
+    protected  void num(ActionEvent e){
+        System.out.println("1");
+    }
+}
